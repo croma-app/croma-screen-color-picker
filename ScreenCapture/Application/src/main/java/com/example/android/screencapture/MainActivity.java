9584a4b9d0b0b984c447c996dfd89a7bdfbf0c08
kamal <kkjoshi.it@gmail.com>
2021-01-09 21:32:49 +0530

    working floating button poc

--- ScreenCapture/Application/src/main/java/com/example/android/screencapture/MainActivity.java
+++ ScreenCapture/Application/src/main/java/com/example/android/screencapture/MainActivity.java
@@ -1,33 +1,37 @@
 /*
-* Copyright 2013 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 package com.example.android.screencapture;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ViewAnimator;
-
-import com.example.android.common.activities.SampleActivityBase;
-import com.example.android.common.logger.Log;
-import com.example.android.common.logger.LogFragment;
-import com.example.android.common.logger.LogWrapper;
-import com.example.android.common.logger.MessageOnlyLogFilter;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * A simple launcher activity containing a summary sample description, sample log and a custom
@@ -36,30 +40,48 @@ import com.example.android.common.logger.MessageOnlyLogFilter;
  * For devices with displays with a width of 720dp or greater, the sample log is always visible,
  * on other devices it's visibility is controlled by an item on the Action Bar.
  */
-public class MainActivity extends SampleActivityBase {
+public class MainActivity extends FragmentActivity {
 
     public static final String TAG = "MainActivity";
+    private static final int DRAW_OVER_OTHER_APPS = 2;
 
     // Whether the Log Fragment is currently shown
     private boolean mLogShown;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
+      /*  if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             ScreenCaptureFragment fragment = new ScreenCaptureFragment();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
-        }
+        }*/
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, DRAW_OVER_OTHER_APPS);
+
+       // startService(new Intent(MainActivity.this, FloatingColorPickerService.class));
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(Settings.canDrawOverlays(this)) {
+            startService(new Intent(MainActivity.this, FloatingColorPickerService.class).putExtra("activity_background", true));
+            finish();
+        }
     }
 
     @Override
@@ -69,6 +91,32 @@ public class MainActivity extends SampleActivityBase {
         logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
 
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DRAW_OVER_OTHER_APPS) {
+            if (Settings.canDrawOverlays(this)) {
+                //Freely draw over other apps
+                System.out.println("hello");
+                ImageView iV = findViewById(R.id.color_view);
+                int strokeWidth = 5;
+                int strokeColor = Color.parseColor("#03dc13");
+                int fillColor = Color.parseColor("#ff0000");
+                GradientDrawable gD = new GradientDrawable();
+                gD.setColor(fillColor);
+                gD.setShape(GradientDrawable.OVAL);
+                gD.setStroke(strokeWidth, strokeColor);
+                iV.setBackground(gD);
+                iV.setVisibility(View.VISIBLE);
+            }
+        } else { //Permission is not available
+            Toast.makeText(this,
+                    "Draw over other app permission not available. Closing the application",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
 }
--- ScreenCapture/Application/src/main/java/com/example/android/screencapture/ScreenCaptureFragment.java
+++ ScreenCapture/Application/src/main/java/com/example/android/screencapture/ScreenCaptureFragment.java
@@ -32,7 +32,6 @@ import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.common.logger.Log;
